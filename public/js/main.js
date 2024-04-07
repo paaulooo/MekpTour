6a592838e0d7d5bef839b5f1af39b456f3fd1ea2
Paulo <115840174+paaulooo@users.noreply.github.com>
2024-04-07 10:52:19 -0300

    form

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,17 @@
 alert('ATENÇÃO ESTE SITE FOI FEITO COMO UM TCC, PLANOS, CADASTRO, PREÇOS, SÃO MERAMENTE ILUSTRATIVOS');
+
+
+
+// formulario
+
+let senha = document.getElementById("senha");
+let senhaConfirma = document.getElementById("confsenha");
+
+function confirmaSenha(){
+    if(senha.value != senhaConfirma.value){
+        senhaConfirma.setCustomValidity('Senhas Diferentes!!');
+    }else{
+        senhaConfirma.setCustomValidity('');
+    }
+}
+senhaConfirma.addEventListener('input', confirmaSenha);
